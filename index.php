d18183a5b7b133bec6545fe58549bd160e38871f
Izik <49079739+izik4004@users.noreply.github.com>
2020-08-08 19:26:57 +0100

    completed form

--- index.php
+++ index.php
@@ -1,8 +1,76 @@
 <?php
 
+$fnameErr = $lnameErr= $emailErr = $fcolorErr= $departmentErr= $passwordErr = $genderErr ='';
+$fname = $lname = $email = $fcolor = $department = $password = $gender = "";
+//firstname validation
 
 
 
+    if (empty($_POST["fname"])) {
+      $fnameErr = "First name is required";
+    } else {
+      $name = test_input($_POST["fname"]);
+      // check if name only contains letters and whitespace
+      if (!preg_match("/^[a-zA-Z ]*$/",$fname)) {
+        $fnameErr = "Only letters and white space allowed";
+        $error = true;
+      }
+    }
+    
+    if (empty($_POST["lname"])) {
+        $lnameErr = "Last name is required";
+      } else {
+        $name = test_input($_POST["lname"]);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z ]*$/",$lname)) {
+          $lnameErr = "Only letters and white space allowed";
+          
+        }
+    }
+
+    //email validation
+    if (empty($_POST["email"])) {
+      $emailErr = "Email is required";
+    } else {
+      $email = test_input($_POST["email"]);
+      // check if e-mail address is well-formed
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Invalid email format";
+        
+      }
+    }
+
+    if (empty($_POST["password"])) {
+        $passwordErr = "Password is required";
+      } else {
+        $passwordErr = test_input($_POST["password"]);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z ]*$/",$password)) {
+          $passwordErr = "Only letters and white space allowed";
+          $error = true;
+        }
+    }
+    
+   
+    if (empty($_POST["gender"])) {
+      $genderErr = "Gender is required";
+    } else {
+      $gender = test_input($_POST["gender"]);
+    }
+  
+  
+
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  
+  
+
+    
+  };
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,10 +83,13 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <title>Sign-Up Form</title>
 </head>
+<style type="text/css">
+        .error{ color: red; }
+        .success{ color: green; }
+    </style>
+
 <body>
-    
-</body>
-</html>
+
 <div class="container">
     <div class="col-8">
         <h2>Sign-Up</h2>
@@ -37,82 +108,54 @@
       
             <p>
                 <label>First Name</label><br />
-                <input  
-                <?php              
-                    if(isset($_SESSION['first_name'])){
-                        echo "value=" . $_SESSION['first_name'];                                                             
-                    }                
-                ?>
-                type="text" class="form-control" name="first_name" placeholder="First Name" />
-            </p>
+                <input  type="text" class="form-control" name="fname" placeholder="First Name"/>                             
+                <span class="error"><?php echo $fnameErr; ?></span>
+                    <?php
+                    if (empty($_POST["fname"])) {
+                        $fnameErr = "First name is required";
+                    } else {
+                        $name = test_input($_POST["fname"]);
+                        // check if name only contains letters and whitespace
+                        if (!preg_match("/^[a-zA-Z ]*$/",$fname)) {
+                        $fnameErr = "Only letters and white space allowed";
+                        }
+                    }
+                    ?>
+
             <p>
                 <label>Last Name</label><br />
-                <input
-                <?php              
-                    if(isset($_SESSION['last_name'])){
-                        echo "value=" . $_SESSION['last_name'];                                                             
-                    }                
-                ?>
-                type="text" class="form-control" name="last_name" placeholder="Last Name"  />
+                <input type="text" class="form-control" name="lname" placeholder="Last Name" required />
+                <span class="error"><?php echo $lnameErr; ?></span>
             </p>
             <p>
                 <label>Email</label><br />
-                <input
-                
-                <?php              
-                    if(isset($_SESSION['email'])){
-                        echo "value=" . $_SESSION['email'];                                                             
-                    }                
-                ?>
-
-                type="text" class="form-control" name="email" placeholder="Email"  />
+                <input type="text" class="form-control" name="email" placeholder="Email" required/>
+               <span class="error"><?php echo $emailErr; ?></span>
+           
             </p>
 
             <p>
                 <label>Password</label><br />
-                           
-            <input type="password" class="form-control" name="password" placeholder="Password"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{15,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 15 or more characters" required>
+                <input type="text" class="form-control" name="password" placeholder="Password" required/>                    
+                <span class="error"><?php echo $passwordErr; ?></span>
             </p>
         
             <p>
                 <label>Date of Birth</label><br />
-                <input 
-                
-                <?php              
-                  if(isset($_SESSION['dob'])){
-                      echo "value=" . $_SESSION['dob'];                                                             
-                  }                
-                ?>
-                type="date" class="form-control" name="dob" id="datepicker"/>
-                
+                <input type="date" class="form-control" name="dob" id="datepicker"/>  
             </p>
             <p>
             <label>Department</label><br />
-                <select class="form-control" name="department" >
+                <select class="form-control" name="department"/>
                     <option value="">Select One</option>
                     <option
-                    <?php              
-                        if(isset($_SESSION['department']) && $_SESSION['department'] == 'IT'){
-                            echo "selected";                                                           
-                        }                
-                    ?>
-                    
-                    >IT</option>
+                   >IT</option>
                     <option
-                    <?php              
-                        if(isset($_SESSION['department']) && $_SESSION['department'] == 'HR'){
-                            echo "selected";                                                           
-                        }                
-                    ?>
+                    
                                
                     >HR</option>
                     <option
-                    <?php              
-                        if(isset($_SESSION['department']) && $_SESSION['department'] == 'Stuff'){
-                            echo "selected";                                                           
-                        }                
-                    ?>
-                    
+                   
                     
                     >Stuff</option>
                     </select>
@@ -120,36 +163,26 @@
                 <p>
                 <p>
                 <label>Favorite Colour</label><br />
-                <input
-                <?php              
-                    if(isset($_SESSION['favorite_color'])){
-                        echo "value=" . $_SESSION['favorite_color'];                                                             
-                    }                
-                ?>
-                type="color" class="form-control" name="favorite_color" placeholder="favorite_color"  />
+                <input type="color"  name="fcolor" />
+                Click on the box to choose your favorite colour
             </p>
             <p>
-                <label>Gender</label><br />
-                <input type="checkbox" id="male" name="male" value="male">
-                <?php if (isset($gender) && $gender=="male") echo "checked";?>
-                <label for="male">Male</label>
-                <input type="checkbox" id="female" name="female" value="female">
-                <?php if (isset($gender) && $gender=="female") echo "checked";?>
-                <label for="female"> Female</label><br>
-        
-                
-            </p>
-
-                </p>           
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="male">
+                <label class="form-check-label" for="inlineRadio1">Male</label>
+                </div>
+                <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="female">
+                <label class="form-check-label" for="inlineRadio2">Female</label>
+                </div>
+                     
                 <p>
-                <button class="btn btn-sm btn-success" type="submit">Register</button>
+                <button class="btn btn-sm btn-success" type="submit" name="submitted">Register</button>
                 </p>
-            <p>
-                    <a href="forgot.php">Forgot Password</a><br />
-                    
-            </p>
+         
         </form>
 
     </div>
 
 </div>
+
